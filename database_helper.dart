c7e4b03629848e6d6eeebbabdbe3c73bd3e48f6e
pingmypoo <132787185+pingmypoo@users.noreply.github.com>
2026-01-13 16:05:03 +0700

    Update database_helper.dart

--- database_helper.dart
+++ database_helper.dart
@@ -6,31 +6,39 @@ class DatabaseHelper {
   static final DatabaseHelper instance = DatabaseHelper.init();
   static Database? _database;
 
-  // pivate constructor
   DatabaseHelper.init();
-
-  Future _createTable(Database db, int version) async {
-    await db.execute(''''
-    CREATE TABLE tasks (
-    idINTEGER PRIMARY KET AUTOINCREMENT,
-    title TEXT NOT NULL,
-    description TEXT,
-    isDone INTEGER NOT NULL
-    )
-    ''');
-  }
-
-  Futer<Database> _initDB(String filePath) async {
-    final dbPath = await getDatabasesPath();
-    final path = join(dbPath, filePath);
-
-    return await openDatabase(path, version: 1, onCreate: _createTable);
-  }
 
   Future<Database> get database async {
     if (_database != null) return _database!;
     _database = await _initDB('tasks.db');
     return _database!;
+  }
+
+  Future<Database> _initDB(String filePath) async {
+    final dbPath = await getDatabasesPath();
+    final path = join(dbPath, filePath);
+
+    return await openDatabase(
+      path,
+      version: 1,
+      onCreate: _createDB,
+    );
+  }
+
+  Future _createDB(Database db, int version) async {
+    await db.execute('''
+    CREATE TABLE tasks (
+      id INTEGER PRIMARY KEY AUTOINCREMENT,
+      title TEXT NOT NULL,
+      description TEXT,
+      isDone INTEGER NOT NULL
+    )
+    ''');
+  }
+
+  Future<int> insertTask(Task task) async {
+    final db = await instance.database;
+    return await db.insert('tasks', task.toMap());
   }
 
   Future<List<Task>> readAllTasks() async {
@@ -39,18 +47,18 @@ class DatabaseHelper {
     return result.map((json) => Task.fromMap(json)).toList();
   }
 
-  Future<int> deleteTask(int id) async {
-    final db = await instance.database;
-    return await db.delete('tasks', where: 'id = ?', whereArgs: [id]);
-  }
-
   Future<int> updateTask(Task task) async {
     final db = await instance.database;
     return await db.update(
-      'tasks'
-      ,task.toMap(),
+      'tasks',
+      task.toMap(),
       where: 'id = ?',
       whereArgs: [task.id],
-      )
+    );
+  }
+
+  Future<int> deleteTask(int id) async {
+    final db = await instance.database;
+    return await db.delete('tasks', where: 'id = ?', whereArgs: [id]);
   }
 }
